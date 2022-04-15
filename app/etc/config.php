90c0b49ed408ce8a98a8d37b193580154485637b
Lucas <lucaspereira.desenvolvedor@gmail.com>
2022-04-15 00:37:36 -0300

    Add module Slide Banner

--- app/etc/config.php
+++ app/etc/config.php
@@ -296,6 +296,7 @@ return [
         'Klarna_Kp' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
+        'Sparsh_Banner' => 1,
         'Temando_ShippingRemover' => 1,
         'Vertex_Tax' => 1,
         'Vertex_AddressValidation' => 1,
